aaaca33230d33ec0a900cf9d58b90a9d1096d93f
Jeremy <thestyle1202@gmail.com>
2014-11-03 21:03:56 +0800

    Comment proj1.cpp for doxygen

--- proj1.cpp
+++ proj1.cpp
@@ -2,6 +2,14 @@
 #include "console.hpp"
 #include "parser.hpp"
 
+/// @file
+/// @author Jeremy
+/// @version 1.0
+/// @section DESCRIPTION
+///
+/// This is project 1 RAS program.
+
+/// @brief Generate a RAS through Server with policy of Console. Then, execute run of server to wait connection.
 int main(int argc, char *argv[])
 {
 	typedef Console<SimpleParser> Console_t;
