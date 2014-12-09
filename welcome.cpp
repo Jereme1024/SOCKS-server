5b85d23c3e56f029c5330a08dde3587d24ad9586
Jeremy <thestyle1202@gmail.com>
2014-12-09 15:46:30 +0800

    Add welcome.cpp

---
+++ welcome.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Content-Type: text/plain";
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout << "Welcome!";
+	std::cout << "\n";
+
+	return 0;
+}
