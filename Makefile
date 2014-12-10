244bf12175094f5d5ba4a1ee9f50dda1f3dc3580
Jeremy <thestyle1202@gmail.com>
2014-12-10 20:21:57 +0800

    Workable RBS (linux version)

--- Makefile
+++ Makefile
@@ -1,13 +1,23 @@
 CC=g++
-CPPFLAGS=-std=c++11 -O2 -lpthread
-EXEC=singleServer multipleServer
+CPPFLAGS=-std=c++11 -O2
+EXEC=httpd *.cgi
 
-all: 
-	#$(CC) $(CPPFLAGS) part1/singleServer.cpp -o singleServer
-	#$(CC) $(CPPFLAGS) part2/multipleServer.cpp -o multipleServer
-	$(CC) $(CPPFLAGS) proj2_single.cpp -o singleServer
-	$(CC) $(CPPFLAGS) proj2_multiple.cpp -o multipleServer
-	
+all: hello welcome printenv hw3 httpd.cpp
+	#$(CC) $(CPPFLAGS) proj2_single.cpp -o singleServer
+	#$(CC) $(CPPFLAGS) proj2_multiple.cpp -o multipleServer
+	$(CC) $(CPPFLAGS) httpd.cpp -o httpd
+
+hello: hello.cpp
+	$(CC) $(CPPFLAGS) hello.cpp -o hello.cgi
+
+welcome: welcome.cpp
+	$(CC) $(CPPFLAGS) welcome.cpp -o welcome.cgi
+
+printenv: printenv.cpp
+	$(CC) $(CPPFLAGS) printenv.cpp -o printenv.cgi
+
+hw3: hw3.cpp
+	$(CC) $(CPPFLAGS) hw3.cpp -o hw3.cgi
 
 install:
 	cp -f /bin/ls /bin/cat ras/bin/
---
+++ client.hpp
@@ -0,0 +1,111 @@
+#ifndef _CLIENT_HPP_
+#define _CLIENT_HPP_
+
+/// @file
+/// @author Jeremy
+/// @version 1.0
+/// @section DESCRIPTION
+///
+/// The client class is able to handle socket operation to build a server. It accepts a Service policy with "run" interface.
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <functional>
+
+#include <vector>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+const int MAX_CONNECTION = 5;
+const int MAX_CLIENT = 31;
+const int MAX_NAME = 32;
+
+
+template <class Service>
+class Client : public Service
+{
+public:
+	int sockfd; // listen fd
+	int portno;
+	char *ip;
+	struct sockaddr_in server_addr;
+	struct hostent *server;
+
+public:
+	/// @brief This method is used to initialize a server by following socket -> bind -> listen(...).
+	/// @param port A port number with a default value 5487.
+	Client(char *ip_in, int port = 5487)
+		: ip(ip_in)
+		, portno(port)
+	{
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd < 0)
+		{
+			std::cerr << "Server socket open failed.\n";
+			exit(EXIT_FAILURE);
+		}
+
+		server = gethostbyname(ip);
+		if (server == NULL) {
+			std::cerr << "Server host by name failed.\n";
+			exit(EXIT_FAILURE);
+		}
+
+		memset(&server_addr, '0', sizeof(server_addr));
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_addr = *((struct in_addr *)server->h_addr);
+		server_addr.sin_port = htons(portno);
+
+		connect_persistly();
+
+		Service::enter(sockfd, server_addr);
+	}
+
+	void connect_once()
+	{
+		if (connect(sockfd, (sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+		{
+			std::cerr << "Connect failed.\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	void connect_persistly()
+	{
+		// Non-blocking setting
+		int flags = fcntl(sockfd, F_GETFL, 0);
+		fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+
+		while (connect(sockfd, (sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+		{
+			std::cerr << "Connect failed. Going to retry in 1 second\n";
+			sleep(1);
+		}
+	}
+
+
+	/// @brief This method is used to accept clients and perform a Service run operation. It will control the fork/wait(...) operations at the same time.
+	void run()
+	{
+		Service::routine(sockfd);
+	}
+
+	~Client()
+	{
+		std::cerr << "Client with fd " << sockfd << " close.\n";
+	}
+};
+#endif
